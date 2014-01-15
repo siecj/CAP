bc0e032201e552f6412fb0eaa4f46f1966abda0e
Jiu Chen <jiuchen@live.com>
2014-01-15 20:30:04 +0800

    add common data statisitc component

---
+++ 1_0/dataprocessor/datastatistic.py
@@ -0,0 +1,52 @@
+import logging
+import unittest
+import csv
+from datetime import *
+
+class DataStatistic():
+    """docstring for DataStatistic"""
+
+    def group_source_by_month(self, dictlist, fidname):
+        """ group source dictionary list by month
+
+        dictlist: source dictionary list
+        fidname: fid in date type, used to group
+        return: a list of dictionary list
+        """
+        result = {}
+        for item in dictlist:
+            monthstr = item[fidname].strftime('%Y-%m')
+            if result.has_key(monthstr):
+                result[monthstr].append(item)
+            else:
+                result[monthstr] = [item]
+
+        return result
+
+    def group_source_by_week(self, dictlist, fidname):
+        pass
+
+class _UT(unittest.TestCase):
+    """docstring for _UT"""
+
+    def setUp(self):
+        self.dictlist = []
+        f = open('temp\ticktes_before_3299.csv', 'r')
+        reader = csv.DictReader(f, dialect='excel-tab')
+        for row in reader:
+            row['Created'] = datetime.strptime(row['Created'], '%Y-%m-%d %H:%M:%S')
+            self.dictlist.append(row)
+        f.close()
+        logging.debug('Load dictionary list finish. Total {0} dictionarys'.format(len(self.dictlist)))
+
+    def test1(self):
+        stat = DataStatistic()
+        result = stat.group_source_by_month(self.dictlist, 'Created')
+        keylist = result.keys()
+        logging.debug(sorted(keylist)
+        self.assertEqual(42, len(result))
+
+if __name__ == '__main__':
+    logging.basicConfig(level='DEBUG')
+    unittest.main()
+        
