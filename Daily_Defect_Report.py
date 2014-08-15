d4ecbe53250e43a6a106f19623d5daa31bb83e74
Jiu Chen <jiuchen@live.com>
2014-08-15 09:52:16 +0800

    Update daily defect report logic

--- Daily_Defect_Report.py
+++ Daily_Defect_Report.py
@@ -187,10 +187,10 @@ class Daily_Defect_Report():
 		cvg_file=path+"CVG_ALL.xml"
 		server={ 'server': 'http://www.iajira.amers.ime.reuters.com'}
 		auth=('zhe.wang','alreadyJIRA6!')
-		cva_core_jsl="component = 'CVACORE' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate < startOfDay() ORDER BY Created DESC"
-		cva_venue_jsl="component = 'CVA' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate < startOfDay() ORDER BY Created DESC"
-		che_jsl="project in ('ERT VA-CHE','ERT CHE-CD','ERT NTS-R','ERT UPA-CHE') AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate < startOfDay() ORDER BY Created DESC"
-		scw_jsl="component = 'ERTSCW' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate < startOfDay() ORDER BY Created DESC"
+		cva_core_jsl="component = 'CVACORE' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate <= currentLogin() ORDER BY Created DESC"
+		cva_venue_jsl="component = 'CVA' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate <= currentLogin() ORDER BY Created DESC"
+		che_jsl="project in ('ERT VA-CHE','ERT CHE-CD','ERT NTS-R','ERT UPA-CHE') AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate <= currentLogin() ORDER BY Created DESC"
+		scw_jsl="component = 'ERTSCW' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= startOfDay(-1d) AND createdDate <= currentLogin() ORDER BY Created DESC"
 		
 		#Lists of defects/enhancements for each products
 	
