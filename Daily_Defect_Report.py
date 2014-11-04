669e6c9eca8eae2ccdcd67a3792c9b399e12333d
Jiu Chen <jiuchen@live.com>
2014-11-04 15:30:46 +0800

    update daily defect report

--- Daily_Defect_Report.py
+++ Daily_Defect_Report.py
@@ -38,7 +38,7 @@ class Daily_Defect_Report():
 		return result
 
 	def Get_JIRA_Defects(self,server,auth,jsl):
-		jira=JIRA(server,auth)
+		jira=JIRA(server)
 		issues=jira.search_issues(jsl,maxResults=2000,fields='key,summary,assignee,reporter,status,created,components')
 		return issues
 		
@@ -138,15 +138,15 @@ class Daily_Defect_Report():
 		cva_venue_jsl_amer="component = 'CVA' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND category = 'Elektron Collections - Americas' ORDER BY created DESC"
 		cva_venue_jsl_emea="component = 'CVA' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND category = 'Elektron Collections - EMEA' ORDER BY created DESC"
 		cva_venue_jsl_asia="component = 'CVA' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND category = 'Elektron Collections - Asia' ORDER BY created DESC"
-		che_jsl="project in ('ERT VA-CHE','ERT CHE-CD','ERT NTS-R','ERT UPA-CHE') AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') ORDER BY created DESC"
+		che_jsl="project in ('ERT VA-CHE','ERT CHE-CD','ERT NTS-R','ERT UPA-CHE','DelphX on Elektron ','ERT Silver Fix') AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') ORDER BY created DESC"
 		scw_jsl="component = 'ERTSCW' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') ORDER BY created DESC"
 		
 		#Lists of defects/enhancements for each products
 		cvg_defects=self.Filter_All_Open(cvg_file)
 	
-		cva_core_jira=self.Get_JIRA_Defects(server,auth,cva_core_jsl)
+		cva_core_jira=self.Get_JIRA_Defects(server,cva_core_jsl)
 
-		cva_venue_amer=self.Get_JIRA_Defects(server,auth,cva_venue_jsl_amer)
+		cva_venue_amer=self.Get_JIRA_Defects(server,cva_venue_jsl_amer)
 
 		cva_venue_emea=self.Get_JIRA_Defects(server,auth,cva_venue_jsl_emea)
 
@@ -189,7 +189,7 @@ class Daily_Defect_Report():
 		auth=('zhe.wang','all2BJ6!')
 		cva_core_jsl="component = 'CVACORE' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= '%s' AND createdDate <= '%s' ORDER BY Created DESC" %(StartDate,EndDate)
 		cva_venue_jsl="component = 'CVA' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= '%s' AND createdDate <= '%s' ORDER BY Created DESC" %(StartDate,EndDate)
-		che_jsl="project in ('ERT VA-CHE','ERT CHE-CD','ERT NTS-R','ERT UPA-CHE') AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= '%s' AND createdDate <= '%s' ORDER BY Created DESC" %(StartDate,EndDate)
+		che_jsl="project in ('ERT VA-CHE','ERT CHE-CD','ERT NTS-R','ERT UPA-CHE','DelphX on Elektron ','ERT Silver Fix') AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= '%s' AND createdDate <= '%s' ORDER BY Created DESC" %(StartDate,EndDate)
 		scw_jsl="component = 'ERTSCW' AND issuetype in (Bug,Improvement) AND status in ('Open','In Progress','In Review') AND createdDate >= '%s' AND createdDate <= '%s' ORDER BY Created DESC" %(StartDate,EndDate)
 		
 		#Lists of defects/enhancements for each products
